436168e3a84a73d825c5c0fb3f4164b788740eab
jxdflores25 <jxdflores25@gmail.com>
2024-02-16 20:42:44 -0500

    Linea de tiempo de certificados

---
+++ src/Data/CertificacionesDatos.js
@@ -0,0 +1,26 @@
+export const CERTIFICATE = [
+  {
+    time: "Febrero 2024",
+    title: "Data Analytics Essentials",
+    organization: "Cisco",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque laboriosam illo dolorum, voluptatem labore libero perspiciatis porromodi dolore quae ipsa amet maiores id vel consequuntur autem fuga ab?Reiciendis.",
+    link: "https://www.credly.com/badges/39b534df-cca7-4e58-964d-09d1989be336/linked_in_profile",
+  },
+  {
+    time: "Enero 2024",
+    title: "Data Analysis with Python",
+    organization: "FreeCodeCamp",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque laboriosam illo dolorum, voluptatem labore libero perspiciatis porromodi dolore quae ipsa amet maiores id vel consequuntur autem fuga ab?Reiciendis.",
+    link: "https://www.freecodecamp.org/certification/jxdflores25/data-analysis-with-python-v7",
+  },
+  {
+    time: "Octubre 2023",
+    title: "Relational Database",
+    organization: "FreeCodeCamp",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque laboriosam illo dolorum, voluptatem labore libero perspiciatis porromodi dolore quae ipsa amet maiores id vel consequuntur autem fuga ab?Reiciendis.",
+    link: "https://www.freecodecamp.org/certification/jxdflores25/relational-database-v8",
+  },
+];
